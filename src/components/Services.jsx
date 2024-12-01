4a92819d38922c5e341b95653b0d40af7e98057c
nitish <nirish@192.168.1.7>
2024-12-02 03:18:06 +0530

    carousel added on services page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import img1 from "../assets/3.jpg";
+import img2 from "../assets/4.jpg";
 
 const Services = () => {
-  return <div className="services">Services</div>;
+  return (
+    <div className="services">
+      <Carousel>
+        <div>
+          <img src={img1} alt="item1" />
+          <p>Full Stack</p>
+        </div>
+        <div>
+          <img src={img2} alt="item1" />
+          <p>AI</p>
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Services;
